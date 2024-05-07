5cfba336a3e8fa50f27b2bd31cd46f11d97df93c
ShallowGreen123 <2608653986@qq.com>
2024-05-07 18:11:37 +0800

    Add some demos

--- lvgl_examples/T_EPD47/ui_epd47.c
+++ lvgl_examples/T_EPD47/ui_epd47.c
@@ -1,55 +1,345 @@
 #include "ui_epd47.h"
 #include "assets/assets.h"
+//************************************[ Other fun ]******************************************
+void scr_back_btn_create(lv_obj_t *parent, lv_event_cb_t cb)
+{
+    lv_obj_t * btn = lv_btn_create(parent);
+    lv_obj_set_style_pad_all(btn, 0, 0);
+    lv_obj_set_height(btn, 50);
+    lv_obj_align(btn, LV_ALIGN_TOP_LEFT, 15, 15);
+    lv_obj_set_style_border_width(btn, 0, LV_PART_MAIN);
+    lv_obj_set_style_shadow_width(btn, 0, LV_PART_MAIN);
+    lv_obj_set_style_bg_color(btn, lv_color_hex(EPD_COLOR_BG), LV_PART_MAIN);
+    lv_obj_add_event_cb(btn, cb, LV_EVENT_CLICKED, NULL);
 
-void epd_create0(lv_obj_t *parent) {
-    lv_obj_t *scr5_cont = lv_obj_create(parent);
-    lv_obj_set_size(scr5_cont, lv_pct(100), lv_pct(100));
-    lv_obj_set_style_bg_color(scr5_cont, lv_color_hex(0xffffff), LV_PART_MAIN);
-    lv_obj_set_scrollbar_mode(scr5_cont, LV_SCROLLBAR_MODE_OFF);
-    lv_obj_set_style_border_width(scr5_cont, 0, LV_PART_MAIN);
-    // lv_obj_set_style_pad_all(scr5_cont, 0, LV_PART_MAIN);
-    // lv_obj_set_flex_flow(scr5_cont, LV_FLEX_FLOW_ROW_WRAP);
+    lv_obj_t *label2 = lv_label_create(btn);
+    lv_obj_align(label2, LV_ALIGN_LEFT_MID, 0, 0);
+    lv_obj_set_style_text_color(label2, lv_color_hex(EPD_COLOR_TEXT), LV_PART_MAIN);
+    lv_label_set_text(label2, " " LV_SYMBOL_LEFT);
 
-    lv_obj_t * img1 = lv_img_create(scr5_cont);
-    lv_obj_t * img2 = lv_img_create(scr5_cont);
-    lv_obj_t * img3 = lv_img_create(scr5_cont);
-    lv_obj_t * img4 = lv_img_create(scr5_cont);
-    lv_obj_t * img5 = lv_img_create(scr5_cont);
+    lv_obj_t *label = lv_label_create(parent);
+    lv_obj_align_to(label, btn, LV_ALIGN_OUT_RIGHT_MID, 10, 0);
+    lv_obj_set_style_text_color(label, lv_color_hex(EPD_COLOR_TEXT), LV_PART_MAIN);
+    lv_label_set_text(label, "Back");
+    lv_obj_add_flag(label, LV_OBJ_FLAG_CLICKABLE);
+    lv_obj_add_event_cb(label, cb, LV_EVENT_CLICKED, NULL);
+}
 
-    lv_img_set_src(img1, &img_clock);
-    lv_img_set_src(img2, &img_lora);
-    lv_img_set_src(img3, &img_sd_card);
-    lv_img_set_src(img4, &img_setting);
-    lv_img_set_src(img5, &img_test);
+//************************************[ screen 0 ]****************************************** menu
+#if 1
+
+#define MENU_ICON_NUM  (6)
+#define MENU_CONT_HIGH (LCD_VER_SIZE * 0.80)
+
+lv_obj_t *scr0_cont;
+struct menu_icon {
+    const void *icon_src;
+    const char *icon_str;
+};
+
+const struct menu_icon icon_buf[MENU_ICON_NUM] = {
+    {&img_clock, "clock"}, 
+    {&img_lora, "lora"},
+    {&img_sd_card, "sd card"},
+    {&img_setting, "setting"},
+    {&img_test, "test"},
+    {&img_wifi, "wifi"},
+};
+
+static void menu_btn_event(lv_event_t *e)
+{
+    int data = (int)e->user_data;
+    if(e->code == LV_EVENT_CLICKED) {
+        printf("%s is clicked.\n", icon_buf[data].icon_str);
+        switch (data) {
+            case 0: scr_mgr_push(SCREEN1_ID, false); break;
+            case 1: scr_mgr_push(SCREEN2_ID, false); break;
+            case 2: scr_mgr_push(SCREEN3_ID, false); break;
+            case 3: scr_mgr_push(SCREEN4_ID, false); break;
+            case 4: scr_mgr_push(SCREEN5_ID, false); break;
+            case 5: scr_mgr_push(SCREEN6_ID, false); break;
+            default:
+                break;
+        }
+    }
+}
+
+static void create0(lv_obj_t *parent) 
+{
+    lv_obj_t *scr0_cont = lv_obj_create(parent);
+    lv_obj_set_size(scr0_cont, lv_pct(100), MENU_CONT_HIGH);
+    lv_obj_set_style_bg_color(scr0_cont, lv_color_hex(0xffffff), LV_PART_MAIN);
+    lv_obj_set_scrollbar_mode(scr0_cont, LV_SCROLLBAR_MODE_OFF);
+    lv_obj_set_style_border_width(scr0_cont, 0, LV_PART_MAIN);
+    lv_obj_set_style_radius(scr0_cont, 0, LV_PART_MAIN);
+    lv_obj_set_style_pad_all(scr0_cont, 0, LV_PART_MAIN);
+    lv_obj_align(scr0_cont, LV_ALIGN_BOTTOM_MID, 0, 0);
+
+    int row_n = 2;
+    int col_n = 5;
+    lv_obj_t *img_buf[MENU_ICON_NUM] = {0};
+    lv_obj_t *lab_buf[MENU_ICON_NUM] = {0};
     
-    lv_obj_align(img1, LV_ALIGN_LEFT_MID, 10, 0);
-    lv_obj_align(img2, LV_ALIGN_LEFT_MID, 150, 0);
-    lv_obj_align(img3, LV_ALIGN_LEFT_MID, 300, 0);
-    lv_obj_align(img4, LV_ALIGN_LEFT_MID, 450, 0);
-    lv_obj_align(img5, LV_ALIGN_LEFT_MID, 600, 0);
+    for(int i = 0; i < MENU_ICON_NUM; i++) {
+        lv_obj_t *img = lv_img_create(scr0_cont);
+        lv_img_set_src(img, icon_buf[i].icon_src);
+        lv_obj_add_flag(img, LV_OBJ_FLAG_CLICKABLE);
+        lv_obj_add_event_cb(img, menu_btn_event, LV_EVENT_CLICKED, (void *)i);
+        lv_obj_t *label = lv_label_create(scr0_cont);
+        lv_label_set_text(label, icon_buf[i].icon_str);
+        img_buf[i] = img;
+        lab_buf[i] = label;
+    }
 
-    lv_obj_add_flag(img1, LV_OBJ_FLAG_CLICKABLE);
-    lv_obj_add_flag(img2, LV_OBJ_FLAG_CLICKABLE);
-    lv_obj_add_flag(img3, LV_OBJ_FLAG_CLICKABLE);
-    lv_obj_add_flag(img4, LV_OBJ_FLAG_CLICKABLE);
-    lv_obj_add_flag(img5, LV_OBJ_FLAG_CLICKABLE);
+    lv_coord_t x_gap = (LCD_HOR_SIZE-(img_clock.header.w * col_n)) / (col_n+1);
+    lv_coord_t y_gap = (MENU_CONT_HIGH / 5);
+
+    printf("x=%d, y=%d\n", x_gap, y_gap);
+
+    // row 1
+    lv_obj_align_to(img_buf[0], scr0_cont, LV_ALIGN_TOP_LEFT, x_gap, 0);
+    for(int i = 1; i < col_n; i++) {
+        lv_obj_align_to(img_buf[i], img_buf[i-1], LV_ALIGN_OUT_RIGHT_MID, x_gap, 0);
+    }
+
+    if(MENU_ICON_NUM > col_n) {
+        // row 2
+        lv_obj_align_to(img_buf[col_n], img_buf[0], LV_ALIGN_OUT_BOTTOM_MID, 0, y_gap);
+        for(int i = col_n+1; i < MENU_ICON_NUM; i++) {
+            lv_obj_align_to(img_buf[i], img_buf[i-1], LV_ALIGN_OUT_RIGHT_MID, x_gap, 0);
+        }
+    }
+
+    for(int i = 0; i < MENU_ICON_NUM; i++) {
+        lv_obj_align_to(lab_buf[i], img_buf[i], LV_ALIGN_OUT_BOTTOM_MID, 0, 10);
+    }
 }
 static void entry0(void) { }
 static void exit0(void) { }
 static void destroy0(void) { }
 
 static scr_lifecycle_t screen0 = {
-    .create = epd_create0,
+    .create = create0,
     .entry =   entry0,
     .exit  =   exit0,
     .destroy = destroy0,
 };
+#endif
+//************************************[ screen 1 ]****************************************** clock
+#if 1
+lv_obj_t *scr1_cont;
 
+static lv_obj_t * meter;
+
+static void set_value(void * indic, int32_t v)
+{
+    lv_meter_set_indicator_end_value(meter, indic, v);
+}
+
+static void scr1_btn_event_cb(lv_event_t * e)
+{
+    if(e->code == LV_EVENT_CLICKED){
+        scr_mgr_pop(false);
+    }
+}
+
+static void create1(lv_obj_t *parent) {
+    meter = lv_meter_create(parent);
+    lv_obj_set_size(meter, 400, 400);
+    lv_obj_center(meter);
+    lv_obj_set_style_border_width(meter, 2, LV_PART_MAIN);
+    lv_obj_set_style_border_color(meter, lv_color_black(), LV_PART_MAIN);
+    lv_obj_set_style_shadow_width(meter, 0, LV_PART_MAIN);
+
+    /*Create a scale for the minutes*/
+    /*61 ticks in a 360 degrees range (the last and the first line overlaps)*/
+    lv_meter_scale_t * scale_min = lv_meter_add_scale(meter);
+    lv_meter_set_scale_ticks(meter, scale_min, 61, 3, 10, lv_palette_main(LV_PALETTE_GREY));
+    lv_meter_set_scale_range(meter, scale_min, 0, 60, 360, 270);
+
+    /*Create another scale for the hours. It's only visual and contains only major ticks*/
+    lv_meter_scale_t * scale_hour = lv_meter_add_scale(meter);
+    lv_meter_set_scale_ticks(meter, scale_hour, 12, 0, 0, lv_palette_main(LV_PALETTE_GREY));               /*12 ticks*/
+    lv_meter_set_scale_major_ticks(meter, scale_hour, 1, 3, 20, lv_color_black(), 25);    /*Every tick is major*/
+    lv_meter_set_scale_range(meter, scale_hour, 1, 12, 330, 300);       /*[1..12] values in an almost full circle*/
+
+    LV_IMG_DECLARE(img_hand)
+
+    /*Add a the hands from images*/
+    lv_meter_indicator_t * indic_min = lv_meter_add_needle_img(meter, scale_min, &img_hand, 5, 5);
+    lv_meter_indicator_t * indic_hour = lv_meter_add_needle_img(meter, scale_min, &img_hand, 5, 5);
+
+    /*Create an animation to set the value*/
+    // lv_anim_t a;
+    // lv_anim_init(&a);
+    // lv_anim_set_exec_cb(&a, set_value);
+    // lv_anim_set_values(&a, 0, 60);
+    // lv_anim_set_repeat_count(&a, LV_ANIM_REPEAT_INFINITE);
+    // lv_anim_set_time(&a, 2000);     /*2 sec for 1 turn of the minute hand (1 hour)*/
+    // lv_anim_set_var(&a, indic_min);
+    // lv_anim_start(&a);
+
+    // lv_anim_set_var(&a, indic_hour);
+    // lv_anim_set_time(&a, 24000);    /*24 sec for 1 turn of the hour hand*/
+    // lv_anim_set_values(&a, 0, 60);
+    // lv_anim_start(&a);
+    
+    // back
+    scr_back_btn_create(parent, scr1_btn_event_cb);
+}
+static void entry1(void) { }
+static void exit1(void) { }
+static void destroy1(void) { }
+
+static scr_lifecycle_t screen1 = {
+    .create = create1,
+    .entry = entry1,
+    .exit  = exit1,
+    .destroy = destroy1,
+};
+#endif
+//************************************[ screen 2 ]****************************************** lora
+#if 1
+lv_obj_t *scr2_cont;
+
+static void scr2_btn_event_cb(lv_event_t * e)
+{
+    if(e->code == LV_EVENT_CLICKED){
+        
+        scr_mgr_pop(false);
+    }
+}
+
+static void create2(lv_obj_t *parent) {
+    scr_back_btn_create(parent, scr2_btn_event_cb);
+}
+static void entry2(void) { }
+static void exit2(void) { }
+static void destroy2(void) { }
+
+static scr_lifecycle_t screen2 = {
+    .create = create2,
+    .entry = entry2,
+    .exit  = exit2,
+    .destroy = destroy2,
+};
+#endif
+//************************************[ screen 3 ]****************************************** sd_card
+#if 1
+lv_obj_t *scr3_cont;
+
+static void scr3_btn_event_cb(lv_event_t * e)
+{
+    if(e->code == LV_EVENT_CLICKED){
+        
+        scr_mgr_pop(false);
+    }
+}
+
+static void create3(lv_obj_t *parent) {
+    scr_back_btn_create(parent, scr3_btn_event_cb);
+}
+static void entry3(void) { }
+static void exit3(void) { }
+static void destroy3(void) { }
+
+static scr_lifecycle_t screen3 = {
+    .create = create3,
+    .entry = entry3,
+    .exit  = exit3,
+    .destroy = destroy3,
+};
+#endif
+//************************************[ screen 4 ]****************************************** setting
+#if 1
+lv_obj_t *scr4_cont;
+
+static void scr4_btn_event_cb(lv_event_t * e)
+{
+    if(e->code == LV_EVENT_CLICKED){
+        
+        scr_mgr_pop(false);
+    }
+}
+
+static void create4(lv_obj_t *parent) {
+    scr_back_btn_create(parent, scr4_btn_event_cb);
+}
+static void entry4(void) { }
+static void exit4(void) { }
+static void destroy4(void) { }
+
+static scr_lifecycle_t screen4 = {
+    .create = create4,
+    .entry = entry4,
+    .exit  = exit4,
+    .destroy = destroy4,
+};
+#endif
+//************************************[ screen 5 ]****************************************** test
+#if 1
+lv_obj_t *scr5_cont;
+
+static void scr5_btn_event_cb(lv_event_t * e)
+{
+    if(e->code == LV_EVENT_CLICKED){
+        
+        scr_mgr_pop(false);
+    }
+}
+
+static void create5(lv_obj_t *parent) {
+    scr_back_btn_create(parent, scr5_btn_event_cb);
+}
+static void entry5(void) { }
+static void exit5(void) { }
+static void destroy5(void) { }
+
+static scr_lifecycle_t screen5 = {
+    .create = create5,
+    .entry = entry5,
+    .exit  = exit5,
+    .destroy = destroy5,
+};
+#endif
+//************************************[ screen 6 ]****************************************** wifi
+#if 1
+lv_obj_t *scr6_cont;
+
+static void scr6_btn_event_cb(lv_event_t * e)
+{
+    if(e->code == LV_EVENT_CLICKED){
+        
+        scr_mgr_pop(false);
+    }
+}
+
+static void create6(lv_obj_t *parent) {
+    scr_back_btn_create(parent, scr6_btn_event_cb);
+}
+static void entry6(void) { }
+static void exit6(void) { }
+static void destroy6(void) { }
+
+static scr_lifecycle_t screen6 = {
+    .create = create6,
+    .entry = entry6,
+    .exit  = exit6,
+    .destroy = destroy6,
+};
+#endif
+
+//************************************[ UI ENTRY ]******************************************
 void ui_epd47_entry(void)
 {
     scr_mgr_init();
     scr_mgr_set_bg_color(EPD_COLOR_BG);
     scr_mgr_register(SCREEN0_ID, &screen0);
+    scr_mgr_register(SCREEN1_ID, &screen1);
+    scr_mgr_register(SCREEN3_ID, &screen3);
+    scr_mgr_register(SCREEN4_ID, &screen4);
+    scr_mgr_register(SCREEN5_ID, &screen5);
+    scr_mgr_register(SCREEN6_ID, &screen6);
 
     scr_mgr_switch(SCREEN0_ID, false); // set root screen
     scr_mgr_set_anim(LV_SCR_LOAD_ANIM_OVER_LEFT, LV_SCR_LOAD_ANIM_OVER_LEFT, LV_SCR_LOAD_ANIM_OVER_LEFT);
