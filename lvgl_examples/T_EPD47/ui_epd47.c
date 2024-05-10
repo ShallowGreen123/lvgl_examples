02da18b0aabe588b87a9b6313036606ec34b1cd1
ShallowGreen123 <2608653986@qq.com>
2024-05-10 16:13:51 +0800

    perf：Optimize the clock date display UI

--- lvgl_examples/T_EPD47/ui_epd47.c
+++ lvgl_examples/T_EPD47/ui_epd47.c
@@ -19,6 +19,7 @@ void scr_back_btn_create(lv_obj_t *parent, const char *text, lv_event_cb_t cb)
 
     lv_obj_t *label = lv_label_create(parent);
     lv_obj_align_to(label, btn, LV_ALIGN_OUT_RIGHT_MID, 10, 0);
+    lv_obj_set_style_text_font(label, &Font_Mono_Bold_30, LV_PART_MAIN);
     lv_obj_set_style_text_color(label, lv_color_hex(EPD_COLOR_TEXT), LV_PART_MAIN);
     lv_label_set_text(label, text);
     lv_obj_add_flag(label, LV_OBJ_FLAG_CLICKABLE);
@@ -127,6 +128,7 @@ static scr_lifecycle_t screen0 = {
 #endif
 //************************************[ screen 1 ]****************************************** clock
 #if 1
+static lv_obj_t *calendar;
 static lv_obj_t * meter;
 static lv_timer_t *get_timer = NULL;
 static lv_meter_indicator_t * indic_min;
@@ -134,8 +136,10 @@ static lv_meter_indicator_t * indic_hour;
 static lv_obj_t *clock_time;
 static lv_obj_t *clock_data;
 static lv_obj_t *clock_ap;
+static lv_obj_t *clock_month;
 
 static const char *week_list_en[7] = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
+static const char * month_names_def[12] = LV_CALENDAR_DEFAULT_MONTH_NAMES;
 
 static void set_value(void * indic, int32_t v)
 {
@@ -154,65 +158,105 @@ static void create1(lv_obj_t *parent) {
     clock_time = lv_label_create(parent);
     clock_data = lv_label_create(parent);
     clock_ap = lv_label_create(parent);
+    clock_month = lv_label_create(parent);
+
+    lv_obj_set_style_border_width(clock_data, 2, 0);
+    lv_obj_set_style_pad_top(clock_data, 30, 0);
+    lv_obj_set_style_border_side(clock_data, LV_BORDER_SIDE_TOP, LV_PART_MAIN);
 
     lv_obj_set_style_text_font(clock_time, &Font_Mono_Bold_90, LV_PART_MAIN);
-    lv_obj_set_style_text_font(clock_data, &Font_Mono_Bold_30, LV_PART_MAIN);
+    lv_obj_set_style_text_font(clock_data, &Font_Mono_Bold_25, LV_PART_MAIN);
     lv_obj_set_style_text_font(clock_ap, &Font_Mono_Bold_30, LV_PART_MAIN);
+    lv_obj_set_style_text_font(clock_month, &Font_Mono_Bold_30, LV_PART_MAIN);
 
     lv_label_set_text_fmt(clock_time, "%02d:%02d", 10, 19);
-    lv_label_set_text_fmt(clock_data, "%04d/%02d/%02d Sun", 2024, 5, 8);
+    lv_label_set_text_fmt(clock_data, "%04d-%02d-%02d Sun", 2024, 5, 8);
     lv_label_set_text_fmt(clock_ap, "%s", "P.M.");
+    lv_label_set_text_fmt(clock_month, "%s", month_names_def[0]);
 
-    meter = lv_meter_create(parent);
-    lv_obj_set_size(meter, 400, 400);
-    lv_obj_set_style_border_width(meter, 2, LV_PART_MAIN);
-    lv_obj_set_style_border_color(meter, lv_color_black(), LV_PART_MAIN);
-    lv_obj_set_style_shadow_width(meter, 0, LV_PART_MAIN);
+    calendar = lv_calendar_create(parent);
+    lv_obj_set_size(calendar, 380, 350);
+    lv_obj_align(calendar, LV_ALIGN_RIGHT_MID, -50, 27);
+    // lv_obj_add_event_cb(calendar, event_handler, LV_EVENT_ALL, NULL);
+    // lv_obj_set_style_radius(calendar, 20, LV_PART_MAIN);
+    lv_obj_set_style_text_font(calendar, &Font_Geist_Bold_20, LV_PART_MAIN);
 
-    /*Create a scale for the minutes*/
-    /*61 ticks in a 360 degrees range (the last and the first line overlaps)*/
-    lv_meter_scale_t * scale_min = lv_meter_add_scale(meter);
-    lv_meter_set_scale_ticks(meter, scale_min, 61, 3, 10, lv_palette_main(LV_PALETTE_GREY));
-    lv_meter_set_scale_range(meter, scale_min, 0, 60, 360, 270);
+    lv_obj_set_style_border_width(calendar, 0, LV_PART_MAIN);
+    lv_obj_set_style_border_width(lv_calendar_get_btnmatrix(calendar), 0, LV_PART_ITEMS);
+    lv_obj_set_style_border_side(lv_calendar_get_btnmatrix(calendar), LV_BORDER_SIDE_TOP, LV_PART_MAIN);
 
-    /*Create another scale for the hours. It's only visual and contains only major ticks*/
-    lv_meter_scale_t * scale_hour = lv_meter_add_scale(meter);
-    lv_meter_set_scale_ticks(meter, scale_hour, 12, 0, 0, lv_palette_main(LV_PALETTE_GREY));               /*12 ticks*/
-    lv_meter_set_scale_major_ticks(meter, scale_hour, 1, 3, 20, lv_color_black(), 25);    /*Every tick is major*/
-    lv_meter_set_scale_range(meter, scale_hour, 1, 12, 330, 300);       /*[1..12] values in an almost full circle*/
+    lv_calendar_set_today_date(calendar, 2024, 5, 9);
+    lv_calendar_set_showed_date(calendar, 2024, 5);
 
-    /*Add a the hands from images*/
-    indic_hour = lv_meter_add_needle_img(meter, scale_min, &img_hand, 5, 5);
-    indic_min = lv_meter_add_needle_img(meter, scale_min, &img_hand_sec, 5, 5);
+
+
+    static lv_point_t line_points[] = { {LCD_HOR_SIZE/2, 0}, {LCD_HOR_SIZE/2, LCD_VER_SIZE-150}};
+
+    /*Create style*/
+    static lv_style_t style_line;
+    lv_style_init(&style_line);
+    lv_style_set_line_width(&style_line, 2);
+    lv_style_set_line_color(&style_line, lv_color_black());
+    lv_style_set_line_rounded(&style_line, true);
+
+    /*Create a line and apply the new style*/
+    lv_obj_t * line1;
+    line1 = lv_line_create(parent);
+    lv_line_set_points(line1, line_points, 2);     /*Set the points*/
+    lv_obj_add_style(line1, &style_line, 0);
+    lv_obj_set_align(line1, LV_ALIGN_LEFT_MID);
+
+    // meter = lv_meter_create(parent);
+    // lv_obj_set_size(meter, 400, 400);
+    // lv_obj_set_style_border_width(meter, 2, LV_PART_MAIN);
+    // lv_obj_set_style_border_color(meter, lv_color_black(), LV_PART_MAIN);
+    // lv_obj_set_style_shadow_width(meter, 0, LV_PART_MAIN);
+
+    // /*Create a scale for the minutes*/
+    // /*61 ticks in a 360 degrees range (the last and the first line overlaps)*/
+    // lv_meter_scale_t * scale_min = lv_meter_add_scale(meter);
+    // lv_meter_set_scale_ticks(meter, scale_min, 61, 3, 10, lv_palette_main(LV_PALETTE_GREY));
+    // lv_meter_set_scale_range(meter, scale_min, 0, 60, 360, 270);
+
+    // /*Create another scale for the hours. It's only visual and contains only major ticks*/
+    // lv_meter_scale_t * scale_hour = lv_meter_add_scale(meter);
+    // lv_meter_set_scale_ticks(meter, scale_hour, 12, 0, 0, lv_palette_main(LV_PALETTE_GREY));               /*12 ticks*/
+    // lv_meter_set_scale_major_ticks(meter, scale_hour, 1, 3, 20, lv_color_black(), 25);    /*Every tick is major*/
+    // lv_meter_set_scale_range(meter, scale_hour, 1, 12, 330, 300);       /*[1..12] values in an almost full circle*/
+
+    // /*Add a the hands from images*/
+    // indic_hour = lv_meter_add_needle_img(meter, scale_min, &img_hand, 5, 5);
+    // indic_min = lv_meter_add_needle_img(meter, scale_min, &img_hand_sec, 5, 5);
     
 
-    /*Create an animation to set the value*/
-    lv_anim_t a;
-    lv_anim_init(&a);
-    lv_anim_set_exec_cb(&a, set_value);
-    lv_anim_set_values(&a, 0, 60);
-    lv_anim_set_repeat_count(&a, LV_ANIM_REPEAT_INFINITE);
-    lv_anim_set_time(&a, 2000);     /*2 sec for 1 turn of the minute hand (1 hour)*/
-    lv_anim_set_var(&a, indic_min);
-    lv_anim_start(&a);
+    // /*Create an animation to set the value*/
+    // lv_anim_t a;
+    // lv_anim_init(&a);
+    // lv_anim_set_exec_cb(&a, set_value);
+    // lv_anim_set_values(&a, 0, 60);
+    // lv_anim_set_repeat_count(&a, LV_ANIM_REPEAT_INFINITE);
+    // lv_anim_set_time(&a, 2000);     /*2 sec for 1 turn of the minute hand (1 hour)*/
+    // lv_anim_set_var(&a, indic_min);
+    // lv_anim_start(&a);
 
-    lv_anim_set_var(&a, indic_hour);
-    lv_anim_set_time(&a, 24000);    /*24 sec for 1 turn of the hour hand*/
-    lv_anim_set_values(&a, 0, 60);
-    lv_anim_start(&a);
+    // lv_anim_set_var(&a, indic_hour);
+    // lv_anim_set_time(&a, 24000);    /*24 sec for 1 turn of the hour hand*/
+    // lv_anim_set_values(&a, 0, 60);
+    // lv_anim_start(&a);
     
     // back
     scr_back_btn_create(parent, "Clock", scr1_btn_event_cb);
 }
 static void entry1(void) {
-    lv_obj_align(meter, LV_ALIGN_RIGHT_MID, -100, 0);
+    // lv_obj_align(meter, LV_ALIGN_RIGHT_MID, -100, 0);
 
     lv_obj_set_style_text_align(clock_time, LV_TEXT_ALIGN_CENTER, LV_PART_MAIN);
     lv_obj_set_style_text_align(clock_data, LV_TEXT_ALIGN_CENTER, LV_PART_MAIN);
     lv_obj_set_style_text_align(clock_ap, LV_TEXT_ALIGN_CENTER, LV_PART_MAIN);
-    lv_obj_align(clock_time, LV_ALIGN_LEFT_MID, 80, -50);
+    lv_obj_align(clock_time, LV_ALIGN_LEFT_MID, 70, -50);
+    lv_obj_align_to(clock_data, clock_time, LV_ALIGN_OUT_BOTTOM_MID, 0, 30);
     lv_obj_align_to(clock_ap, clock_time, LV_ALIGN_OUT_RIGHT_MID, 0, 20);
-    lv_obj_align_to(clock_data, clock_time, LV_ALIGN_OUT_BOTTOM_MID, 25, 30);
+    lv_obj_align_to(clock_month, calendar, LV_ALIGN_OUT_TOP_RIGHT, 0, -5);
 }
 static void exit1(void) { }
 static void destroy1(void) {
@@ -256,6 +300,8 @@ static scr_lifecycle_t screen2 = {
 //************************************[ screen 3 ]****************************************** sd_card
 #if 1
 lv_obj_t *scr3_cont;
+lv_obj_t *sd_info;
+lv_obj_t *ui_photos_img = NULL;
 
 static void read_img_btn_event(lv_event_t * e)
 {
@@ -310,12 +356,33 @@ static void create3(lv_obj_t *parent) {
         scr3_add_img_btn();
     }
 
+    lv_obj_t *lab1;
+    if(true) {
+        // ui_if_epd_read_from_SD();
+        ui_photos_img = lv_img_create(lv_layer_top());
+        lv_obj_align(ui_photos_img, LV_ALIGN_BOTTOM_MID, 0, 0);
+
+        sd_info = lv_label_create(parent);
+        lv_obj_set_style_text_font(sd_info, &Font_Mono_Bold_30, LV_PART_MAIN);
+        lv_label_set_text(sd_info, "SD GALLERY"); 
+    } else {
+        sd_info = lv_label_create(parent);
+        lv_obj_set_style_text_font(sd_info, &Font_Mono_Bold_90, LV_PART_MAIN);
+        lv_label_set_text(sd_info, "NO FIND SD CARD!"); 
+    }
+
     // back
     scr_back_btn_create(parent, "SD", scr3_btn_event_cb);
 }
 static void entry3(void) 
 {
     lv_obj_align(scr3_cont, LV_ALIGN_BOTTOM_MID, 0, 0);
+
+    if(true) {
+        lv_obj_align(sd_info, LV_ALIGN_TOP_MID, 0, 22);
+    } else {
+        lv_obj_center(sd_info);
+    }
 }
 static void exit3(void) { }
 static void destroy3(void) { }
@@ -383,6 +450,19 @@ static scr_lifecycle_t screen5 = {
 #if 1
 lv_obj_t *scr6_cont;
 
+static void event_handler(lv_event_t * e)
+{
+    lv_event_code_t code = lv_event_get_code(e);
+    lv_obj_t * obj = lv_event_get_current_target(e);
+
+    if(code == LV_EVENT_VALUE_CHANGED) {
+        lv_calendar_date_t date;
+        if(lv_calendar_get_pressed_date(obj, &date)) {
+            LV_LOG_USER("Clicked date: %02d.%02d.%d", date.day, date.month, date.year);
+        }
+    }
+}
+
 static void scr6_btn_event_cb(lv_event_t * e)
 {
     if(e->code == LV_EVENT_CLICKED){
@@ -392,6 +472,7 @@ static void scr6_btn_event_cb(lv_event_t * e)
 }
 
 static void create6(lv_obj_t *parent) {
+    // back
     scr_back_btn_create(parent, "Wifi", scr6_btn_event_cb);
 }
 static void entry6(void) { }
@@ -409,6 +490,9 @@ static scr_lifecycle_t screen6 = {
 //************************************[ UI ENTRY ]******************************************
 void ui_epd47_entry(void)
 {
+    lv_disp_t *disp = lv_disp_get_default();
+    disp->theme = lv_theme_mono_init(disp, false, LV_FONT_DEFAULT);
+
     scr_mgr_init();
     scr_mgr_set_bg_color(EPD_COLOR_BG);
     scr_mgr_register(SCREEN0_ID, &screen0);
