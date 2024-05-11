8a317064396926b889d135cd5bdb16dbf994b4e8
ShallowGreen123 <2608653986@qq.com>
2024-05-11 17:31:55 +0800

    T-EPD47：Add lora、wifi、Setting demo

--- lvgl_examples/T_EPD47/ui_epd47.c
+++ lvgl_examples/T_EPD47/ui_epd47.c
@@ -26,6 +26,22 @@ void scr_back_btn_create(lv_obj_t *parent, const char *text, lv_event_cb_t cb)
     lv_obj_add_event_cb(label, cb, LV_EVENT_CLICKED, NULL);
 }
 
+void scr_middle_line(lv_obj_t *parent)
+{
+    static lv_point_t line_points[] = { {LCD_HOR_SIZE/2, 0}, {LCD_HOR_SIZE/2, LCD_VER_SIZE-150}};
+    /*Create style*/
+    static lv_style_t style_line;
+    lv_style_init(&style_line);
+    lv_style_set_line_width(&style_line, 2);
+    lv_style_set_line_color(&style_line, lv_color_black());
+    lv_style_set_line_rounded(&style_line, true);
+    /*Create a line and apply the new style*/
+    lv_obj_t * line1;
+    line1 = lv_line_create(parent);
+    lv_line_set_points(line1, line_points, 2);     /*Set the points*/
+    lv_obj_add_style(line1, &style_line, 0);
+    lv_obj_set_align(line1, LV_ALIGN_LEFT_MID);
+}
 //************************************[ screen 0 ]****************************************** menu
 #if 1
 
@@ -188,23 +204,8 @@ static void create1(lv_obj_t *parent) {
     lv_calendar_set_today_date(calendar, 2024, 5, 9);
     lv_calendar_set_showed_date(calendar, 2024, 5);
 
-
-
-    static lv_point_t line_points[] = { {LCD_HOR_SIZE/2, 0}, {LCD_HOR_SIZE/2, LCD_VER_SIZE-150}};
-
-    /*Create style*/
-    static lv_style_t style_line;
-    lv_style_init(&style_line);
-    lv_style_set_line_width(&style_line, 2);
-    lv_style_set_line_color(&style_line, lv_color_black());
-    lv_style_set_line_rounded(&style_line, true);
-
-    /*Create a line and apply the new style*/
-    lv_obj_t * line1;
-    line1 = lv_line_create(parent);
-    lv_line_set_points(line1, line_points, 2);     /*Set the points*/
-    lv_obj_add_style(line1, &style_line, 0);
-    lv_obj_set_align(line1, LV_ALIGN_LEFT_MID);
+    //---------------------
+    scr_middle_line(parent);
 
     // meter = lv_meter_create(parent);
     // lv_obj_set_size(meter, 400, 400);
@@ -273,6 +274,8 @@ static scr_lifecycle_t screen1 = {
 //************************************[ screen 2 ]****************************************** lora
 #if 1
 static lv_obj_t *scr2_cont;
+static lv_obj_t *scr2_cont_info;
+static lv_obj_t *lora_mode_sw;
 
 static void scr2_btn_event_cb(lv_event_t * e)
 {
@@ -282,11 +285,135 @@ static void scr2_btn_event_cb(lv_event_t * e)
     }
 }
 
-static void create2(lv_obj_t *parent) {
+static void label_buf_fill_20(const char *text, int idx)
+{
+#define ONE_LINE_LEN 35
+    char buf[ONE_LINE_LEN];
+    int text_len = strlen(text);
+    int i = text_len;
+    int digi = 0;
     
+    while(idx > 0) {
+        idx = idx / 10;
+        digi++;
+    }
+    
+    strncpy(buf, ONE_LINE_LEN-3, text);
+
+    while(i < (ONE_LINE_LEN-3)) {
+        buf[i] = '-';
+    };
+#undef ONE_LINE_LEN
+}
+
+static lv_obj_t * scr2_create_label(lv_obj_t *parent)
+{
+    lv_obj_t *label = lv_label_create(parent);
+    lv_obj_set_width(label, LCD_HOR_SIZE/2-50);
+    lv_obj_set_style_text_font(label, &Font_Mono_Bold_25, LV_PART_MAIN);   
+    lv_obj_set_style_border_width(label, 1, LV_PART_MAIN);
+    lv_label_set_long_mode(label, LV_LABEL_LONG_WRAP);
+    return label;
+}
+
+static void create2(lv_obj_t *parent) {
+
+    scr2_cont = lv_obj_create(parent);
+    lv_obj_set_size(scr2_cont, lv_pct(49), lv_pct(85));
+    lv_obj_set_style_bg_color(scr2_cont, lv_color_hex(EPD_COLOR_BG), LV_PART_MAIN);
+    lv_obj_set_scrollbar_mode(scr2_cont, LV_SCROLLBAR_MODE_OFF);
+    lv_obj_clear_flag(scr2_cont, LV_OBJ_FLAG_SCROLLABLE);
+    // lv_obj_set_style_border_width(scr2_cont, 0, LV_PART_MAIN);
+    lv_obj_set_style_pad_all(scr2_cont, 0, LV_PART_MAIN);
+    lv_obj_set_style_pad_hor(scr2_cont, 20, LV_PART_MAIN);
+    lv_obj_set_flex_flow(scr2_cont, LV_FLEX_FLOW_COLUMN);
+    lv_obj_set_style_pad_row(scr2_cont, 10, LV_PART_MAIN);
+    lv_obj_set_style_pad_column(scr2_cont, 5, LV_PART_MAIN);
+    lv_obj_set_align(scr2_cont, LV_ALIGN_BOTTOM_LEFT);
+
+    scr2_cont_info = lv_obj_create(parent);
+    lv_obj_set_size(scr2_cont_info, lv_pct(49), lv_pct(85));
+    lv_obj_set_style_bg_color(scr2_cont_info, lv_color_hex(EPD_COLOR_BG), LV_PART_MAIN);
+    lv_obj_set_scrollbar_mode(scr2_cont_info, LV_SCROLLBAR_MODE_OFF);
+    lv_obj_clear_flag(scr2_cont_info, LV_OBJ_FLAG_SCROLLABLE);
+    // lv_obj_set_style_border_width(scr2_cont_info, 0, LV_PART_MAIN);
+    lv_obj_set_style_pad_all(scr2_cont_info, 0, LV_PART_MAIN);
+    lv_obj_set_style_pad_hor(scr2_cont_info, 10, LV_PART_MAIN);
+    lv_obj_set_flex_flow(scr2_cont_info, LV_FLEX_FLOW_COLUMN);
+    lv_obj_set_style_pad_row(scr2_cont_info, 10, LV_PART_MAIN);
+    lv_obj_set_style_pad_column(scr2_cont_info, 5, LV_PART_MAIN);
+    lv_obj_set_align(scr2_cont_info, LV_ALIGN_BOTTOM_RIGHT);
+
+    //
+    lora_mode_sw = lv_btn_create(parent);
+    lv_obj_set_style_radius(lora_mode_sw, 5, LV_PART_MAIN);
+    lv_obj_set_style_border_width(lora_mode_sw, 2, LV_PART_MAIN);
+    
+    lv_obj_t *label;
+    label = lv_label_create(lora_mode_sw);
+    lv_obj_set_style_text_font(label, &Font_Mono_Bold_25, LV_PART_MAIN);   
+    lv_label_set_text(label, "MODE SW");
+    
+    //---------------------
+    scr_middle_line(parent);
+
+    label = scr2_create_label(scr2_cont);
+    lv_label_set_text_fmt(label, "%s : %s", "Mode", "send");
+
+    label = scr2_create_label(scr2_cont);
+    lv_label_set_text_fmt(label, "%s : %0.1fMHz", "Frequency", 433.5);
+
+    label = scr2_create_label(scr2_cont);
+    lv_label_set_text_fmt(label, "%s : %0.1fKHz", "Bandwidth", 250.0);
+
+    label = scr2_create_label(scr2_cont);
+    lv_label_set_text_fmt(label, "%s : %d", "SpreadingFactor", 10);
+
+    label = scr2_create_label(scr2_cont);
+    lv_label_set_text_fmt(label, "%s : %d", "CodingRate", 6);
+
+    label = scr2_create_label(scr2_cont);
+    lv_label_set_text_fmt(label, "%s : 0x%X", "SyncWord", 0xAB);
+
+    label = scr2_create_label(scr2_cont);
+    lv_label_set_text_fmt(label, "%s : %d", "OutputPower", 10);
+
+    label = scr2_create_label(scr2_cont);
+    lv_label_set_text_fmt(label, "%s : %d", "CurrentLimit", 80);
+
+    label = scr2_create_label(scr2_cont);
+    lv_label_set_text_fmt(label, "%s : %d", "PreambleLength", 15);
+
+
+    // --------------- RIGHT ---------------
+    label = scr2_create_label(scr2_cont_info);
+    lv_obj_set_style_text_align(label, LV_TEXT_ALIGN_CENTER, LV_PART_MAIN);
+    lv_label_set_text(label, "SEND");
+
+    for(int i = 0; i < 10; i++) {
+        label = scr2_create_label(scr2_cont_info);
+        lv_label_set_text_fmt(label, "%d", i);
+    }
+
+    //
+    // for(int i = 0; i < 10; i++) {
+    //     char buf[35];
+    //     lv_snprintf(buf, 35, "%d-%s", i, "Test Label");
+    //     scr2_create_label(scr2_cont, buf);
+        // label = lv_label_create(scr2_cont);
+        // lv_obj_set_width(label, LCD_HOR_SIZE/2-50);
+        // lv_obj_set_style_text_font(label, &Font_Mono_Bold_25, LV_PART_MAIN);   
+        // lv_obj_set_style_border_width(label, 1, LV_PART_MAIN);
+        // lv_label_set_long_mode(label, LV_LABEL_LONG_WRAP);
+        // lv_label_set_text_fmt(label, "1234567890123456789012345678");
+        // lv_label_set_text_fmt(label, "1234567890123456789012345678901234567890");
+    // }
+    // back
     scr_back_btn_create(parent, "Lora", scr2_btn_event_cb);
 }
-static void entry2(void) { }
+static void entry2(void) { 
+    lv_obj_align(lora_mode_sw, LV_ALIGN_TOP_MID, 0, 22);
+}
 static void exit2(void) { }
 static void destroy2(void) { }
 
@@ -306,15 +433,19 @@ lv_obj_t *ui_photos_img = NULL;
 
 static void read_img_btn_event(lv_event_t * e)
 {
-    int data = (int)e->user_data;
-
+    static int idx = 0;
     if(e->code = LV_EVENT_CLICKED) {
-        printf("imgbtn %d\n", data);
+        lv_img_set_src(ui_photos_img, icon_buf[idx].icon_src);
+        idx++;
+        if(idx > sizeof(icon_buf)/sizeof(icon_buf[0])-1) {
+            idx = 0;
+        }
     }
 }
 
 static void scr3_btn_event_cb(lv_event_t * e)
-{
+{   
+    
     if(e->code == LV_EVENT_CLICKED){
         scr_mgr_pop(false);
     }
@@ -326,18 +457,20 @@ static void scr3_add_img_btn(lv_obj_t *parent, const void *src, const char *tetx
     lv_obj_set_size(obj, LCD_HOR_SIZE/11, LCD_HOR_SIZE/11);
     lv_obj_clear_flag(obj, LV_OBJ_FLAG_SCROLLABLE);
     lv_obj_set_style_pad_all(obj, 0, LV_PART_MAIN);
-    lv_obj_set_style_border_width(obj, 1, LV_PART_MAIN);
+    // lv_obj_set_style_border_width(obj, 0, LV_PART_MAIN);
     lv_obj_set_style_shadow_width(obj, 0, LV_PART_MAIN);
 
-    lv_obj_t *btn = lv_img_create(obj);
-    lv_obj_align(btn, LV_ALIGN_CENTER, 0, -10);
-    lv_obj_add_flag(btn, LV_OBJ_FLAG_CLICKABLE);
-    lv_img_set_src(btn, src);
+    lv_obj_t *img = lv_img_create(obj);
+    lv_obj_align(img, LV_ALIGN_CENTER, 0, -10);
+    lv_obj_add_flag(img, LV_OBJ_FLAG_CLICKABLE);
+    lv_img_set_src(img, src);
 
     lv_obj_t *lab = lv_label_create(obj);
     lv_obj_set_style_text_font(lab, &Font_Mono_Bold_20, LV_PART_MAIN);
     lv_label_set_text(lab, tetx);
-    lv_obj_align_to(lab, btn, LV_ALIGN_OUT_BOTTOM_MID, 0, 0);
+    lv_obj_align_to(lab, img, LV_ALIGN_OUT_BOTTOM_MID, 0, 0);
+
+    lv_obj_add_event_cb(img, read_img_btn_event, LV_EVENT_CLICKED, NULL);
 }
 
 static void create3(lv_obj_t *parent) {
@@ -358,27 +491,14 @@ static void create3(lv_obj_t *parent) {
     lv_obj_set_style_bg_color(scr3_cont_img, lv_color_hex(EPD_COLOR_BG), LV_PART_MAIN);
     lv_obj_set_scrollbar_mode(scr3_cont_img, LV_SCROLLBAR_MODE_OFF);
     // lv_obj_set_style_border_width(scr3_cont_img, 0, LV_PART_MAIN);
-    lv_obj_set_style_pad_all(scr3_cont_img, 26, LV_PART_MAIN);
-    lv_obj_set_flex_flow(scr3_cont_img, LV_FLEX_FLOW_ROW_WRAP);
-    lv_obj_set_style_pad_row(scr3_cont_img, 27, LV_PART_MAIN);
-    lv_obj_set_style_pad_column(scr3_cont_img, 27, LV_PART_MAIN);
+    lv_obj_set_style_pad_all(scr3_cont_img, 0, LV_PART_MAIN);
     lv_obj_set_align(scr3_cont_img, LV_ALIGN_BOTTOM_RIGHT);
 
-    //---------------------
-    static lv_point_t line_points[] = { {LCD_HOR_SIZE/2, 0}, {LCD_HOR_SIZE/2, LCD_VER_SIZE-150}};
-    /*Create style*/
-    static lv_style_t style_line;
-    lv_style_init(&style_line);
-    lv_style_set_line_width(&style_line, 2);
-    lv_style_set_line_color(&style_line, lv_color_black());
-    lv_style_set_line_rounded(&style_line, true);
-    /*Create a line and apply the new style*/
-    lv_obj_t * line1;
-    line1 = lv_line_create(parent);
-    lv_line_set_points(line1, line_points, 2);     /*Set the points*/
-    lv_obj_add_style(line1, &style_line, 0);
-    lv_obj_set_align(line1, LV_ALIGN_LEFT_MID);
+    ui_photos_img = lv_img_create(scr3_cont_img);
+    lv_obj_align(ui_photos_img, LV_ALIGN_CENTER, 0, 0);
 
+    //---------------------
+    scr_middle_line(parent);
 
 
     for(int i = 0; i < 10; i++) {
@@ -389,8 +509,6 @@ static void create3(lv_obj_t *parent) {
     lv_obj_t *lab1;
     if(true) {
         // ui_if_epd_read_from_SD();
-        ui_photos_img = lv_img_create(lv_layer_top());
-        lv_obj_align(ui_photos_img, LV_ALIGN_BOTTOM_MID, 0, 0);
 
         sd_info = lv_label_create(parent);
         lv_obj_set_style_text_font(sd_info, &Font_Mono_Bold_30, LV_PART_MAIN);
@@ -406,8 +524,6 @@ static void create3(lv_obj_t *parent) {
 }
 static void entry3(void) 
 {
-    // lv_obj_align(scr3_cont, LV_ALIGN_BOTTOM_MID, 0, 0);
-
     if(true) {
         lv_obj_align(sd_info, LV_ALIGN_TOP_MID, 0, 22);
     } else {
@@ -436,7 +552,36 @@ static void scr4_btn_event_cb(lv_event_t * e)
     }
 }
 
+static void slider_event_cb(lv_event_t * e);
+static lv_obj_t * slider_label;
+
+static void slider_event_cb(lv_event_t * e)
+{
+    lv_obj_t * slider = lv_event_get_target(e);
+    char buf[8];
+    lv_snprintf(buf, sizeof(buf), "%d%%", (int)lv_slider_get_value(slider));
+    lv_label_set_text(slider_label, buf);
+    lv_obj_align_to(slider_label, slider, LV_ALIGN_OUT_BOTTOM_MID, 0, 10);
+}
+
+
 static void create4(lv_obj_t *parent) {
+    /*Create a slider in the center of the display*/
+    lv_obj_t * slider = lv_slider_create(parent);
+    lv_obj_set_size(slider, 500, 40);
+    lv_slider_set_range(slider, 0, 10);
+    lv_obj_set_style_anim_time(slider, 0, LV_PART_MAIN);
+    lv_obj_center(slider);
+    lv_obj_set_style_radius(slider, LV_RADIUS_CIRCLE, LV_PART_MAIN);
+    lv_obj_add_event_cb(slider, slider_event_cb, LV_EVENT_VALUE_CHANGED, NULL);
+
+    /*Create a label below the slider*/
+    slider_label = lv_label_create(parent);
+    lv_label_set_text(slider_label, "0%");
+
+    lv_obj_align_to(slider_label, slider, LV_ALIGN_OUT_BOTTOM_MID, 0, 10);
+
+    // back
     scr_back_btn_create(parent, "Setting", scr4_btn_event_cb);
 }
 static void entry4(void) { }
@@ -468,7 +613,7 @@ lv_obj_t * scr5_imgbtn_create(lv_obj_t *parent, int idx)
     lv_obj_t *obj = lv_obj_create(parent);
     lv_obj_set_size(obj, 120, 150);
     lv_obj_clear_flag(obj, LV_OBJ_FLAG_SCROLLABLE);
-    lv_obj_set_style_border_width(obj, 0, LV_PART_MAIN);
+    // lv_obj_set_style_border_width(obj, 0, LV_PART_MAIN);
     lv_obj_set_style_pad_all(obj, 0, LV_PART_MAIN);
 
     lv_obj_t *img = lv_img_create(obj);
@@ -488,7 +633,7 @@ static void create5(lv_obj_t *parent) {
     lv_obj_set_size(scr5_cont_PASS, lv_pct(49), lv_pct(80));
     lv_obj_set_style_bg_color(scr5_cont_PASS, lv_color_hex(EPD_COLOR_BG), LV_PART_MAIN);
     lv_obj_set_scrollbar_mode(scr5_cont_PASS, LV_SCROLLBAR_MODE_OFF);
-    lv_obj_set_style_border_width(scr5_cont_PASS, 0, LV_PART_MAIN);
+    // lv_obj_set_style_border_width(scr5_cont_PASS, 0, LV_PART_MAIN);
     lv_obj_set_style_pad_all(scr5_cont_PASS, 26, LV_PART_MAIN);
     lv_obj_set_flex_flow(scr5_cont_PASS, LV_FLEX_FLOW_ROW_WRAP);
     lv_obj_set_style_pad_row(scr5_cont_PASS, 27, LV_PART_MAIN);
@@ -499,7 +644,7 @@ static void create5(lv_obj_t *parent) {
     lv_obj_set_size(scr5_cont_FAIL, lv_pct(49), lv_pct(80));
     lv_obj_set_style_bg_color(scr5_cont_FAIL, lv_color_hex(EPD_COLOR_BG), LV_PART_MAIN);
     lv_obj_set_scrollbar_mode(scr5_cont_FAIL, LV_SCROLLBAR_MODE_OFF);
-    lv_obj_set_style_border_width(scr5_cont_FAIL, 0, LV_PART_MAIN);
+    // lv_obj_set_style_border_width(scr5_cont_FAIL, 0, LV_PART_MAIN);
     lv_obj_set_style_pad_all(scr5_cont_FAIL, 26, LV_PART_MAIN);
     lv_obj_set_flex_flow(scr5_cont_FAIL, LV_FLEX_FLOW_ROW_WRAP);
     lv_obj_set_style_pad_row(scr5_cont_FAIL, 27, LV_PART_MAIN);
@@ -529,20 +674,9 @@ static void create5(lv_obj_t *parent) {
     }
 
     //---------------------
-    static lv_point_t line_points[] = { {LCD_HOR_SIZE/2, 0}, {LCD_HOR_SIZE/2, LCD_VER_SIZE-150}};
-    /*Create style*/
-    static lv_style_t style_line;
-    lv_style_init(&style_line);
-    lv_style_set_line_width(&style_line, 2);
-    lv_style_set_line_color(&style_line, lv_color_black());
-    lv_style_set_line_rounded(&style_line, true);
-    /*Create a line and apply the new style*/
-    lv_obj_t * line1;
-    line1 = lv_line_create(parent);
-    lv_line_set_points(line1, line_points, 2);     /*Set the points*/
-    lv_obj_add_style(line1, &style_line, 0);
-    lv_obj_set_align(line1, LV_ALIGN_LEFT_MID);
+    scr_middle_line(parent);
 
+    // back
     scr_back_btn_create(parent, "Test", scr5_btn_event_cb);
 }
 static void entry5(void) { }
@@ -559,6 +693,10 @@ static scr_lifecycle_t screen5 = {
 //************************************[ screen 6 ]****************************************** wifi
 #if 1
 lv_obj_t *scr6_cont;
+lv_obj_t *wifi_st_lab = NULL;
+lv_obj_t *ip_lab;
+lv_obj_t *ssid_lab;
+lv_obj_t *pwd_lab;
 
 static void event_handler(lv_event_t * e)
 {
@@ -582,6 +720,33 @@ static void scr6_btn_event_cb(lv_event_t * e)
 }
 
 static void create6(lv_obj_t *parent) {
+
+    wifi_st_lab = lv_label_create(parent);
+    // lv_obj_set_style_text_color(wifi_st_lab, lv_color_hex(COLOR_TEXT), LV_PART_MAIN);
+    lv_obj_set_style_text_font(wifi_st_lab, &Font_Mono_Bold_30, LV_PART_MAIN);
+    lv_label_set_text(wifi_st_lab, "Wifi Connect");
+    lv_obj_align(wifi_st_lab, LV_ALIGN_TOP_LEFT, 50, 100);
+
+    ip_lab = lv_label_create(parent);
+    // lv_obj_set_style_text_color(ip_lab, lv_color_hex(COLOR_TEXT), LV_PART_MAIN);
+    lv_obj_set_style_text_font(ip_lab, &Font_Mono_Bold_30, LV_PART_MAIN);
+    lv_label_set_text_fmt(ip_lab, "ip: %s", "WiFi.localIP().toString()");
+    lv_obj_align_to(ip_lab, wifi_st_lab, LV_ALIGN_OUT_BOTTOM_LEFT, 0, 10);
+
+    ssid_lab = lv_label_create(parent);
+    // lv_obj_set_style_text_color(ssid_lab, lv_color_hex(COLOR_TEXT), LV_PART_MAIN);
+    lv_obj_set_style_text_font(ssid_lab, &Font_Mono_Bold_30, LV_PART_MAIN);
+    lv_label_set_text_fmt(ssid_lab, "ssid: %s", "wifi_ssid");
+    lv_obj_align_to(ssid_lab, ip_lab, LV_ALIGN_OUT_BOTTOM_LEFT, 0, 10);
+
+    pwd_lab = lv_label_create(parent);
+    // lv_obj_set_style_text_color(pwd_lab, lv_color_hex(COLOR_TEXT), LV_PART_MAIN);
+    lv_obj_set_style_text_font(pwd_lab, &Font_Mono_Bold_30, LV_PART_MAIN);
+    lv_label_set_text_fmt(pwd_lab, "pswd: %s", "wifi_password");
+    lv_obj_align_to(pwd_lab, ssid_lab, LV_ALIGN_OUT_BOTTOM_LEFT, 0, 10);
+
+    //---------------------
+    scr_middle_line(parent);
     // back
     scr_back_btn_create(parent, "Wifi", scr6_btn_event_cb);
 }
@@ -605,13 +770,13 @@ void ui_epd47_entry(void)
 
     scr_mgr_init();
     scr_mgr_set_bg_color(EPD_COLOR_BG);
-    scr_mgr_register(SCREEN0_ID, &screen0);
-    scr_mgr_register(SCREEN1_ID, &screen1);
-    scr_mgr_register(SCREEN2_ID, &screen2);
-    scr_mgr_register(SCREEN3_ID, &screen3);
-    scr_mgr_register(SCREEN4_ID, &screen4);
-    scr_mgr_register(SCREEN5_ID, &screen5);
-    scr_mgr_register(SCREEN6_ID, &screen6);
+    scr_mgr_register(SCREEN0_ID, &screen0); // menu
+    scr_mgr_register(SCREEN1_ID, &screen1); // clock
+    scr_mgr_register(SCREEN2_ID, &screen2); // lora
+    scr_mgr_register(SCREEN3_ID, &screen3); // sd card
+    scr_mgr_register(SCREEN4_ID, &screen4); // setting
+    scr_mgr_register(SCREEN5_ID, &screen5); // test
+    scr_mgr_register(SCREEN6_ID, &screen6); // wifi
 
     scr_mgr_switch(SCREEN0_ID, false); // set root screen
     scr_mgr_set_anim(LV_SCR_LOAD_ANIM_OVER_LEFT, LV_SCR_LOAD_ANIM_OVER_LEFT, LV_SCR_LOAD_ANIM_OVER_LEFT);
